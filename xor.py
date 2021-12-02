f848ec7b966f80d7a7f79193a78f9c8a976112bb
anastasiaxxx <95322927+anastasiaxxx@users.noreply.github.com>
2021-12-02 17:51:55 +0200

    Update xor.py

--- xor.py
+++ xor.py
@@ -1,5 +1,16 @@
 from sys import argv
+if len(argv) != 4:
+    print('USAGE: python3 project.py <password> <input_file> <output_file>')
+    exit()
 
+if not (argv[1].isalnum()):
+    print('ERROR: Password must be a string with lowercase or uppercase letters or numbers.')
+    exit()
+
+if len(argv[1]) < 10 or len(argv[1]) > 15:
+    print('ERROR: Password must have between 10-15 characters.')
+    exit()
+    
 f = open(argv[2], "rb")
 g = open(argv[3], "wb")
 
